58b5dc3dfc30048b7d93c3286b70d3b0cb827c60
陈远 <15000618306@163.com>
2025-02-11 17:54:11 +0800

    删除icon显示

--- docs/.vitepress/components/button/disabled.vue
+++ docs/.vitepress/components/button/disabled.vue
@@ -1,11 +1,11 @@
 <template>
   <cy-button label="禁用按钮" disabled />
   <div style="margin-bottom: 20px;"/>
-  <cy-button label="禁用带图标" disabled :icon="loop" />
+  <!-- <cy-button label="禁用带图标" disabled :icon="loop" /> -->
 </template>
 
 <script setup lang="ts">
-import loop from '../../../../src/assets/icon/loop.svg'
+// import loop from '../../../../src/assets/icon/loop.svg'
 </script>
 
 <style lang="scss" scoped>
--- docs/.vitepress/components/button/with-icon.vue
+++ docs/.vitepress/components/button/with-icon.vue
@@ -1,11 +1,11 @@
 <template>
-  <cy-button label="带图标按钮" :icon="Delete" />
+  <!-- <cy-button label="带图标按钮" :icon="Delete" /> -->
   <div style="margin-bottom: 20px;"/>
-  <cy-button label="自定义图标" :icon="loop" customerClass="with-large-icon" />
+  <!-- <cy-button label="自定义图标" :icon="loop" customerClass="with-large-icon" /> -->
 </template>
 <script setup lang="ts">
-import { Delete } from '@element-plus/icons-vue'
-import loop from '../../../../src/assets/icon/loop.svg'
+// import { Delete } from '@element-plus/icons-vue'
+// import loop from '../../../../src/assets/icon/loop.svg'
 
 </script>
 
