0efbaf5bf20b807e65f97bd32ec688f8aea8b7bb
chenyuan0308 <chenyuan0308@users.noreply.github.com>
2025-02-12 02:42:35 +0000

    deploy: 25de52ceba7c108f64613106be188e8261cd7c77

--- assets/chunks/with-icon.CUozA_eC.js
+++ assets/chunks/with-icon.CUozA_eC.js
@@ -5,15 +5,15 @@ const n=`<template>
 </template>
 <script setup lang="ts">
 import { Delete } from '@element-plus/icons-vue'
-import loop from '../../../../src/assets/icon/loop.svg'
+import loop from '../../../../src/assets/icon/loop.png'
 
 <\/script>
 
 <style lang="scss" scoped>
 :deep() {
   img {
-    width: 30px;
-    height: 30px;
+    width: 20px;
+    height: 20px;
   }
 }
 </style>`;export{n as default};
