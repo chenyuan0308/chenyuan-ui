9c392e72ee0c51ff6b6ea9a29c8edc0e2ff32e9f
chenyuan0308 <chenyuan0308@users.noreply.github.com>
2025-03-03 07:40:35 +0000

    deploy: 4d6f0e33037ef3db2fd3238e10e8f801e29dcd8a

---
+++ assets/chunks/StepsBasic.hD3PqZZa.js
@@ -0,0 +1,28 @@
+const t=`<script setup lang="ts">
+import { ref } from 'vue'
+
+const active = ref(1)
+const steps = [
+  {
+    title: '步骤1',
+    description: '这是第一步的描述'
+  },
+  {
+    title: '步骤2',
+    description: '这是第二步的描述'
+  },
+  {
+    title: '步骤3',
+    description: '这是第三步的描述'
+  }
+]
+<\/script>
+
+<template>
+  <CYSteps :activeStep="active" :stepList="steps" />
+  <div style="margin-top: 20px">
+    <el-button @click="active--" :disabled="active <= 0">上一步</el-button>
+    <el-button @click="active++" :disabled="active >= steps.length">下一步</el-button>
+  </div>
+</template>
+`;export{t as default};
